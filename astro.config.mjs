e0e0fccba7d66e5aa65f1d1b8b13a92e915690a6
Raluca Rusu <cozea.raluca@yahoo.com>
2024-01-17 15:30:24 +0000

    chore: Add sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://stefan-kinetosport.ro",
+  integrations: [tailwind(), sitemap()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
