605f1fe82406f00a9003e7e3378b0db9abab8a45
Raluca Rusu <cozea.raluca@yahoo.com>
2024-01-28 09:50:45 +0000

    feat: Use partytown library for GA4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stefan-kinetosport.ro",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
